74fbad9b536381c44596a22bb30529144966d10e
Hannes <hannes-bachmann@t-online.de>
2023-01-05 12:19:58 +0100

    gamma correction reworked

--- 002_gamma_curve.py
+++ 002_gamma_curve.py
@@ -15,72 +15,43 @@ def read_image(img_path):
     :rtype: ndarray
     """
     resource_path = 'C:\\Users\\Hannes\\PycharmProjects\\CV_exercices\\resources\\' + img_path
-    return cv2.imread(resource_path, cv2.IMREAD_GRAYSCALE)
+    return cv2.imread(resource_path)
 
 
-def gamma_operator(img, gamma):
-    scaled_img = img.copy() / 255
-    for r_i, row in enumerate(scaled_img):
-        for c_i, col_point in enumerate(row):
-            scaled_img[r_i][c_i] = col_point ** gamma # round(col_point ** gamma, 1)
-    return scaled_img
+def gamma_correction(img, gamma):
+    """gamma correction of an image
+
+    :param gamma: correction parameter
+    :type gamma: float
+    :param img: some image
+    :type img: cv2 mat img
+    :return: new gamma corrected image
+    """
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma  # inverted gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(img, table)
 
 
-def histogram(img_f, param_c):
-    hist_results = 0
-    for r_i in range(img_f.shape[0]):
-        for c_i in range(img_f.shape[1]):
-            if img_f[r_i][c_i] == param_c:
-                hist_results += 1
-    return hist_results
+def show(img, gamma_corrected, current_gamma):
+    cv2.imshow("original grayscale", img)
+    cv2.imshow(f"gamma={current_gamma} grayscale", gamma_corrected)
+    cv2.waitKey()
 
 
-def histogram_curve(img):
-    x_c_axis = [i for i in range(100)]
-    y_histogram_axis = [histogram(img_f=img, param_c=x / 100) for x in range(100)]
-    plt.plot(x_c_axis, y_histogram_axis)
-    plt.show()
-    pass
-
-
-def cumulative_distribution_of_colors(img, param_c):
-    c_selector = lambda c2: histogram(img, param_c) if c2 <= param_c else 0
-
-    summed_histogram = sum([sum([c_selector(img[i_row][i_col]) for i_col in range(img.shape[1])]) for i_row in range(img.shape[0])])
-    print(f"c: {param_c}, sum: {summed_histogram}")
-    return summed_histogram / (img.shape[0] * img.shape[1])
-
-
-def cumulative_distribution_of_colors_curve(img):
-    x_c_axis = [i for i in range(10)]
-    y_histogram_axis = [cumulative_distribution_of_colors(img, i / 10) for i in range(10)]
-    plt.plot(x_c_axis, y_histogram_axis)
-    plt.show()
-    pass
+def start():
+    # load grayscale image from file
+    img = read_image('flowers_gray.png')
+    # gamma correction on img
+    gamma = 0.5
+    if gamma != 0:
+        corrected_img = gamma_correction(img, gamma)
+        show(img, corrected_img, gamma)
 
 
 if __name__ == '__main__':
-    # get grayscale image from file
-    img = read_image('flowers_gray.png')
-    # convert image to hsv (in case of using RGB and not GRAY)
-    # img = cv2.cvtColor(img, "RGB2HSV")
-    current_gamma = 0.5
-    gamma_img = gamma_operator(img, current_gamma)
-    # histogram(gamma_img, 0.5)
-    # histogram_curve(gamma_img)
-    scale_percent = 20  # percent of original size
-    width = int(gamma_img.shape[1] * scale_percent / 100)
-    height = int(gamma_img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    # resize image
-    resized = cv2.resize(gamma_img, dim, interpolation=cv2.INTER_AREA)
-    new_resized = resized.copy()
-    for i_row in range(resized.shape[0]):
-        for i_col in range(resized.shape[1]):
-            new_resized[i_row][i_col] = round(resized[i_row][i_col], 1)
+    start()
 
-    cumulative_distribution_of_colors_curve(new_resized)
-
-    cv2.imshow("original grayscale", img)
-    cv2.imshow(f"gamma={current_gamma} grayscale", gamma_img)
-    cv2.waitKey()
